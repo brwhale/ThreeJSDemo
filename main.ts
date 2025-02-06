97da43c33b87ee19807f259cbf5c17cfc8b3aca6
Garrett Skelton <lazorpwnage@gmail.com>
2025-02-05 21:17:08 -0800

    shadows

--- main.ts
+++ main.ts
@@ -15,6 +15,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 3, -10);
 const renderer = new THREE.WebGLRenderer();
+renderer.shadowMap.enabled = true;
 const mousePos = new THREE.Vector2();
 const lookDir = new THREE.Vector3(0,0,1);
 const clock = new THREE.Clock();
@@ -32,7 +33,7 @@ let physicsWorld : LIBAMMO.default.btSoftRigidDynamicsWorld;
 
 function loadModel(modelPath: string, texturePath: string) {
     const texture = texLoader.load(texturePath);
-    const textureMaterial = new THREE.MeshBasicMaterial({
+    const textureMaterial = new THREE.MeshPhongMaterial({
         map: texture,
     });
 
@@ -87,6 +88,7 @@ function updatePhysics( timestep: number ) {
 const rigidBodies : Array<THREE.Object3D>=[];
 let player: LIBAMMO.default.btRigidBody;
 let playerMesh: THREE.Object3D;
+const sun = new THREE.DirectionalLight( 0xffffff, 3 );
 
 function createRigidBody(threeObject : THREE.Object3D, physicsShape : LIBAMMO.default.btCollisionShape, mass: number) {
     const transform = new Ammo.btTransform();
@@ -94,16 +96,12 @@ function createRigidBody(threeObject : THREE.Object3D, physicsShape : LIBAMMO.de
     transform.setOrigin( new Ammo.btVector3( threeObject.position.x, threeObject.position.y, threeObject.position.z ) );
     transform.setRotation( new Ammo.btQuaternion( threeObject.quaternion.x, threeObject.quaternion.y, threeObject.quaternion.z, threeObject.quaternion.w ) );
     const motionState = new Ammo.btDefaultMotionState( transform );
-
     const localInertia = new Ammo.btVector3( 0, 0, 0 );
     physicsShape.calculateLocalInertia( mass, localInertia );
-
     const rbInfo = new Ammo.btRigidBodyConstructionInfo( mass, motionState, physicsShape, localInertia );
     const body = new Ammo.btRigidBody( rbInfo );
     body.setFriction(0.5);
     threeObject.userData.physicsBody = body;
-
-    scene.add( threeObject );
 
     if ( mass > 0 ) {
         rigidBodies.push( threeObject );
@@ -112,32 +110,53 @@ function createRigidBody(threeObject : THREE.Object3D, physicsShape : LIBAMMO.de
     }
 
     physicsWorld.addRigidBody( body );
+    scene.add( threeObject );
+}
+
+function makeBox(position: THREE.Vector3, size: THREE.Vector3, mass: number, color: THREE.ColorRepresentation | undefined) {
+    const geometry = new THREE.BoxGeometry( size.x,size.y,size.z );
+    const material = new THREE.MeshPhongMaterial( {color: color} );    
+    const cube = new THREE.Mesh( geometry, material );
+    cube.receiveShadow = true;
+	cube.castShadow = true;
+    cube.position.copy(position);
+    const cubeShape = new Ammo.btBoxShape(new Ammo.btVector3(size.x,size.y,size.z).op_mul(.5));
+    createRigidBody(cube, cubeShape, mass);
+    return cube;
 }
 
 function createObjects() {
-    const floor = new THREE.Mesh(new THREE.BoxGeometry(100, 1, 100), new THREE.MeshBasicMaterial( { color: "grey"}));
-    floor.position.set(0,-1,0);
-    const floorShape = new Ammo.btBoxShape( new Ammo.btVector3(50,.5,50) );
-    createRigidBody(floor, floorShape, 0);
+    makeBox(new THREE.Vector3(0,-1,0), new THREE.Vector3(100, 1, 100), 0, "grey");
 
     for (let i = -5; i < 5; ++i) {
         for (let j = -5; j < 5; ++j) {
-            const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-            const material = new THREE.MeshBasicMaterial( {color: new THREE.Color().setHSL(.5 + i * 0.1, .85 + j *.1, .5)} );
-            const cube = new THREE.Mesh( geometry, material );
-            cube.position.x = i*1.2;
-            cube.position.z = j*1.2;
-            cube.position.y = 3;
-            const cubeShape = new Ammo.btBoxShape(new Ammo.btVector3(.5,.5,.5));
-            createRigidBody(cube, cubeShape, 4);
+            makeBox(new THREE.Vector3(i*1.2,3,j*1.2), new THREE.Vector3(1, 1, 1), 4,
+                new THREE.Color().setHSL(.5 + i * 0.1, .85 + j *.1, .5));
         }
     }
 
-    playerMesh = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial({color: "green"}));
-    playerMesh.position.set(0, 3, -10);
-    const cubeShape = new Ammo.btBoxShape(new Ammo.btVector3(.5,.5,.5));
-    createRigidBody(playerMesh, cubeShape, 4);
+    playerMesh = makeBox(new THREE.Vector3(0, 3, -10), new THREE.Vector3(1,1,1), 4, "green");
     player = playerMesh.userData.physicsBody;
+
+    sun.position.set( 100, 100, -50 );
+    sun.castShadow = true;
+
+    const dLight = 200;
+    const sLight = 20;
+    sun.shadow.camera.left = -sLight;
+    sun.shadow.camera.right = sLight;
+    sun.shadow.camera.top = sLight;
+    sun.shadow.camera.bottom = -sLight;
+    
+    sun.shadow.camera.near = 70;
+    sun.shadow.camera.far = dLight;
+
+    sun.shadow.mapSize.x = 1024 * 2;
+    sun.shadow.mapSize.y = 1024 * 2;
+
+    sun.target = playerMesh;
+
+    scene.add( sun );
 }
 
 function animate() {
